68d37f690c729b7c1f86318e92d0ad327992da60
slabys <simi.sla@seznam.cz>
2022-10-20 13:19:40 +0200

    init

---
+++ src/main/java/cz/uhk/pproproject/PproProjectApplication.java
@@ -0,0 +1,13 @@
+package cz.uhk.pproproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PproProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PproProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/uhk/pproproject/PproProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.pproproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PproProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
